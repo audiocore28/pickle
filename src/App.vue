492557c73baf01482da78c0a5d610f39938e91a3
jef balmes <jefbalmes@gmail.com>
2026-02-08 17:07:29 +0800

    added platform buttons for horizontal scroll

--- src/App.vue
+++ src/App.vue
@@ -20,22 +20,22 @@ const platforms = [
   },
   {
     name: 'switch',
-    desc: 'Switch Games',
+    desc: 'NSW Console',
   },
   {
-    name: 'ps3',
-    desc: 'PS3 Games',
+    name: 'playstation3',
+    desc: 'PS3 Console',
   },
   {
-    name: 'ps2',
-    desc: 'PS2 Games',
+    name: 'playstation2',
+    desc: 'PS2 Console',
   },
   {
-    name: 'basic',
+    name: 'low-end',
     desc: 'Basic / Low-End',
   },
   {
-    name: '2p',
+    name: '2-players',
     desc: '2 Players',
   },
 ];
@@ -90,6 +90,24 @@ const progressStyle = computed(() => {
       width: progress.value + "%" 
     };
 });
+
+function scrollLeft() {
+  const scrollContainer = document.getElementById('scrollContainer');
+
+  scrollContainer.scrollBy({
+    left: -150,
+    behavior: 'smooth'
+  });
+}
+
+function scrollRight() {
+  const scrollContainer = document.getElementById('scrollContainer');
+
+  scrollContainer.scrollBy({
+    left: 150,
+    behavior: 'smooth'
+  });
+}
 
 onMounted(async () => {
   try {
@@ -154,16 +172,32 @@ onMounted(async () => {
       <!-- / Search -->
 
       <!-- Platforms -->
-      <div class="mx-auto flex items-center md:justify-center overflow-x-scroll space-x-2 p-4 text-xs uppercase whitespace-nowrap scroll-smooth">
-        <button v-for="pf in platforms" @click.prevent="platform = pf.name" 
-          :class="{
-            'bg-neutral-300 text-white' : platform !== pf.name,
-            'bg-green-200 text-green-600 font-semibold' : platform == pf.name,
-          }"
-          class="uppercase flex-shrink-0 px-2 py-2 rounded-full cursor-pointer hover:bg-neutral-600"
-        >
-          {{ pf.desc }}
-        </button>
+      <div class="mx-auto flex items-center md:justify-center space-x-2 p-4 text-xs uppercase whitespace-nowrap scroll-smooth">
+        <button @click="scrollLeft()" id="scrollLeft" class="p-2 mr-2 bg-neutral-300 text-white rounded-full shadow-lg hover:bg-green-600 focus:outline-none md:hidden">
+          <!-- Heroicon: Chevron Left -->
+          <svg xmlns="https://www.w3.org" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+          </svg>
+        </button>  
+
+        <div id="scrollContainer" class="flex overflow-x-scroll items-center lg:justify-center lg:overflow-x-hidden space-x-4 w-full max-w-4xl bg-gray-100 rounded-lg shadow-inner">
+          <button v-for="pf in platforms" @click.prevent="platform = pf.name" 
+            :class="{
+              'bg-neutral-300 text-white' : platform !== pf.name,
+              'bg-green-200 text-green-600' : platform == pf.name,
+            }"
+            class="uppercase flex-shrink-0 px-2 py-2 rounded-full cursor-pointer hover:bg-neutral-600"
+          >
+            {{ pf.desc }}
+          </button>
+        </div>
+
+        <button @click="scrollRight()" id="scrollRight" class="p-2 ml-1 bg-neutral-300 text-white rounded-full shadow-lg hover:bg-green-600 focus:outline-none md:hidden">
+          <!-- Heroicon: Chevron Left -->
+          <svg xmlns="https://www.w3.org" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+          </svg>
+        </button>  
       </div>
       <!-- / Platforms -->
 
