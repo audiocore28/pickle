2eb6740c2b1f47b0b903a15d6e9d5ffebc98ddae
jef balmes <jefbalmes@gmail.com>
2026-03-11 16:14:38 +0800

    SelectedList component changed layout & added dynamic color

--- src/components/SelectedList.vue
+++ src/components/SelectedList.vue
@@ -2,11 +2,10 @@
 import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useGameStore } from '../stores/game';
-import Sort from '@/components/Sort.vue';
 import Screenshot from './Screenshot.vue';
 
 const store = useGameStore();
-const { groupedSelection, device } = storeToRefs(store);
+const { groupedSelection, device, deviceColor } = storeToRefs(store);
 
 const activeTab = ref('all'); 
 </script>
@@ -20,19 +19,18 @@ const activeTab = ref('all');
     
     <!-- Modal -->
     <div class="relative w-full cursor-pointer pointer-events-none transition my-auto mb-1">
-      <div class="w-full py-2 bg-stone-500 cursor-default pointer-events-auto dark:bg-gray-800 relative rounded-xl mx-auto max-w-md border border-stone-500">
+      <div class="w-full cursor-default pointer-events-auto dark:bg-gray-800 relative mx-auto max-w-md">
       
         
         <!-- Header -->
-        <div class="bg-stone-500 px-4 flex items-center justify-between">
-          <h2 class="text-stone-200 text-sm uppercase font-semibold">{{ device }} drive</h2>
-          <Sort />
+        <div :class="deviceColor" class="pb-4 pt-5 rounded-t-xl flex items-center justify-center">
+          <h2 class="text-md uppercase font-semibold">{{ device }} drive</h2>
         </div>
         <div aria-hidden="true" class="border-b dark:border-gray-700 px-2"></div>
         <!-- / Header -->
 
         <!-- Tab navigation links -->
-        <div class="text-xs uppercase text-center border-b border-gray-200 hidden xs:block">
+        <div class="text-xs uppercase text-center border-b border-gray-200 hidden">
           <ul class="flex flex-wrap -mb-px">
             <li class="mr-2">
               <a 
@@ -59,7 +57,7 @@ const activeTab = ref('all');
         <!-- / Tab navigation links -->
         
         <!-- Tab content -->
-        <div class="bg-stone-100 rounded-md px-4 mx-auto max-w-md max-h-[470px] overflow-x-scroll">
+        <div class="bg-stone-100 px-4 mx-auto max-w-md max-h-[470px] overflow-x-scroll">
           <div v-for="(group, groupName) in groupedSelection.list" class="mt-5">
             <h2 v-show="activeTab === 'all'" class="text-stone-600 text-xs font-semibold uppercase mb-3">{{ groupName }}</h2>
 
@@ -67,7 +65,7 @@ const activeTab = ref('all');
               <li v-show="activeTab === game.platform || activeTab === 'all'" v-for="game in group" :key="game.id" class="flex items-center justify-between py-2 border-b border-gray-300">
                 <div class="flex items-center">
                   <!-- <span class="text-lg font-semibold mr-4">1</span> -->
-                  <img :src="game.image" alt="game cover" class="w-8 h-8 rounded-full mr-4 hidden xs:block">
+                  <img :src="game.image" alt="game cover" class="w-8 h-8 rounded-full mr-4">
                   <span class="text-xs text-gray-800 max-w-[200px] xs:max-w-[260px] truncate">{{ game.name }}</span>
                 </div>
 
@@ -96,7 +94,7 @@ const activeTab = ref('all');
           <div class="px-6 py-2">
 
             <div class="grid gap-2 grid-cols-[repeat(auto-fit,minmax(0,1fr))]">
-              <button @click="store.captureElement" type="button" class="cursor-pointer inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-[12px] sm:text-sm text-stone-600 bg-yellow-500 border-stone-300 hover:bg-yellow-400 focus:ring-primary-600 focus:text-primary-600 focus:bg-primary-50 focus:border-primary-600 dark:bg-gray-800 dark:hover:bg-gray-700 dark:border-gray-600 dark:hover:border-gray-500 dark:text-gray-200 dark:focus:text-primary-400 dark:focus:border-primary-400 dark:focus:bg-gray-800">
+              <button @click="store.captureElement" type="button" :class="deviceColor" class="cursor-pointer inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-[12px] sm:text-sm border-stone-300 focus:ring-primary-600 focus:text-primary-600 focus:bg-primary-50 focus:border-primary-600 dark:bg-gray-800 dark:hover:bg-gray-700 dark:border-gray-600 dark:hover:border-gray-500 dark:text-gray-200 dark:focus:text-primary-400 dark:focus:border-primary-400 dark:focus:bg-gray-800">
                 <span class="flex items-center gap-1">
                   <span class="">
                     Take Screenshot
