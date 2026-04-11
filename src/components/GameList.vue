984cdac1ab06eeb2753b5d2a00e7b6df0b40dacf
jef balmes <jefbalmes@gmail.com>
2026-04-11 12:47:09 +0800

    added Highlights component

--- src/components/GameList.vue
+++ src/components/GameList.vue
@@ -3,6 +3,7 @@ import { storeToRefs } from 'pinia';
 import { useGameStore } from '../stores/game';
 import GameDetail from '@/components/GameDetail.vue';
 import GameDetailSkeleton from './GameDetailSkeleton.vue';
+import Highlights from '@/components/Highlights.vue';
 
 const store = useGameStore();
 const { games, selected, filteredGames } = storeToRefs(store);
@@ -12,6 +13,7 @@ const { games, selected, filteredGames } = storeToRefs(store);
   <section class="bg-stone-800 mt-35 sm:mt-30 md:mt-24 lg:mt-10">
     <div class="max-w-screen-2xl mx-auto p-5 sm:p-10 md:p-16">
       <div class="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-4">
+      <Highlights />
   
         <div v-if="games.length" v-for="game in filteredGames" :key="game.id" class="relative cursor-pointer rounded overflow-hidden shadow-lg"
           @click.prevent="store.toggleSelect(game)" 
---
+++ src/components/Highlights.vue
@@ -0,0 +1,135 @@
+<script setup>
+import { ref, onUnmounted } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useGameStore } from '../stores/game';
+import GameDetail from '@/components/GameDetail.vue';
+
+const gameStore = useGameStore();
+const { games, selected, filteredGames } = storeToRefs(gameStore);
+
+const scrollContainerRef = ref(null);
+const isDragging = ref(false);
+let startX, scrollLeft;
+
+const startDragging = (e) => {
+  isDragging.value = false;
+  startX = e.pageX - scrollContainerRef.value.offsetLeft;
+  scrollLeft = scrollContainerRef.value.scrollLeft;
+  
+  // Attach to window to handle dragging outside the element
+  window.addEventListener('mousemove', mouseMoveHandler);
+  window.addEventListener('mouseup', stopDragging);
+};
+
+const mouseMoveHandler = (e) => {
+  isDragging.value = true;
+  if (!isDragging.value) return;
+  e.preventDefault();
+  const x = e.pageX - scrollContainerRef.value.offsetLeft;
+  const walk = (x - startX); // Adjust multiplier for scroll speed
+  scrollContainerRef.value.scrollLeft = scrollLeft - walk;
+};
+
+const stopDragging = () => {
+  setTimeout(() => {
+    isDragging.value = false;
+  }, 100);
+
+  window.removeEventListener('mousemove', mouseMoveHandler);
+  window.removeEventListener('mouseup', stopDragging);
+};
+
+function handleClick(game) {
+  // prevent clicking when dragging
+  if (isDragging.value) return;
+
+  gameStore.toggleSelect(game)
+}
+
+function scrollToLeft() {
+  scrollContainerRef.value.scrollBy({
+    left: -300,
+    behavior: 'smooth'
+  });
+}
+
+function scrollToRight() {
+  scrollContainerRef.value.scrollBy({
+    left: 300,
+    behavior: 'smooth'
+  });
+}
+
+// Cleanup listeners if component is destroyed while dragging
+onUnmounted(stopDragging);
+</script>
+
+<template>
+  <div class="my-10">
+    <h3 class="font-display text-lg font-bold text-stone-300 uppercase mb-1 flex items-center gap-2">Highlights</h3>;
+    
+    <div class="relative">
+      <button @click="scrollToLeft()" class="absolute left-0 top-0 bottom-2 w-10 z-20 flex items-center justify-center bg-transparent transition-opacity cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          :stroke="'#e7e5e4'"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-chevron-left w-5 h-5 text-foreground"
+        >
+          <path d="m15 18-6-6 6-6"></path>
+        </svg>
+      </button>
+      <button @click="scrollToRight()" class="absolute right-0 top-0 bottom-2 w-10 z-20 flex items-center justify-center bg-transparent transition-opacity cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          :stroke="'#e7e5e4'"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-chevron-right w-5 h-5 text-foreground"
+        >
+          <path d="m9 18 6-6-6-6"></path>
+        </svg>
+      </button>
+
+      <!-- container -->
+      <div 
+        ref="scrollContainerRef" 
+        @mousedown="startDragging($event)"
+        class="flex gap-3 overflow-x-auto pb-2 -mx-4 px-4 cursor-grab [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden scrollbar-hide select-none"
+      >
+
+        <!-- loop -->
+        <div v-if="games.length" v-for="game in filteredGames" :key="game.id" class="flex-shrink-0 snap-start rounded-md overflow-hidden transition-all duration-200 bg-card/80 backdrop-blur-sm border w-[130px] sm:w-[140px] border-primary/20 hover:border-primary/40">
+          <div class="relative aspect-[3/4] bg-muted cursor-pointer">
+          
+            <div class="relative cursor-pointer rounded overflow-hidden shadow-lg"
+              @click.stop.prevent="handleClick(game)" 
+              :class="{
+                'bg-stone-500 text-stone-200' : selected.includes(game.id),
+                'hover:text-stone-500 transition duration-500 ease-in-out' : !selected.includes(game.id)
+              }"
+            >
+              <GameDetail :game="game" />
+            </div>
+
+          </div>
+        </div>
+        <!-- / loop -->
+
+      </div>
+      <!-- / container -->
+
+    </div>
+  </div>
+</template>
